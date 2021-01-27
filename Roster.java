7c62eabeec8078976c603fea47e159062dad0c1a
GGirl1971 <GGirl1971@172-0-7-199.lightspeed.brhmal.sbcglobal.net>
2021-01-27 10:35:30 -0700

    changed roster

--- Roster.java
+++ Roster.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Roster {
     private ArrayList<Student> studentList = new ArrayList<>(); // ArrayList containing the list of students
@@ -42,5 +43,58 @@ public class Roster {
     //////////////
     // METHODS //
     /////////////
+    
+    
+    public static void addStudent()
+    {
+    	Scanner userInp = new Scanner(System.in); 
+    	System.out.println("What is the name of the student you would like to add?");
+	    String student = userInp.nextLine();
+	    // Add student to roster
+	    
+	    
+	    	Scanner userInp2 = new Scanner(System.in); 
+	    	System.out.println("What is the name of thr first class for this student");
+		    String course1 = userInp2.nextLine();
+		    
+		    Scanner userInp3 = new Scanner(System.in); 
+		    System.out.println("What is the grade for the first class for this student");
+		    String grade1 = userInp3.nextLine();
+		    
+		    Scanner userInp4 = new Scanner(System.in); 
+	    	System.out.println("What is the name of thr first class for this student");
+		    String course2 = userInp4.nextLine();
+		    
+		    Scanner userInp5 = new Scanner(System.in); 
+		    System.out.println("What is the grade for the first class for this student");
+		    String grade2 = userInp5.nextLine();
+		    
+		    Scanner userInp6 = new Scanner(System.in); 
+	    	System.out.println("What is the name of thr first class for this student");
+		    String course3 = userInp6.nextLine();
+		    
+		    Scanner userInp7 = new Scanner(System.in); 
+		    System.out.println("What is the grade for the first class for this student");
+		    String grade3 = userInp7.nextLine();
+		    
+		    
+		    
+		    //set new student based on user imputted paramaters.
+		    
+	    
+    }
+    
+    public static void deleteStudent()
+    {
+    	//display roster and grades
+    	
+    	Scanner userInp = new Scanner(System.in); 
+    	System.out.println("Please select the roster number of the student you would like to delete");
+	    int student = userInp.nextInt();
+	    
+	    //delete from roster and student list
+    	
+    	
+    }
 
 }
