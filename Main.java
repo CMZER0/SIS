4624ddf1d8b07ea05625d347e571abde1e61dbc4
Cole Merchant <xcmzer0x@gmail.com>
2021-01-29 13:52:32 -0700

    first

--- Main.java
+++ Main.java
@@ -1,16 +1,16 @@
 // Objectives given here: https://docs.google.com/document/d/1VyqRG5wR3q0exFC8klmNL9SVNYCZzdsOd8hYDabRpMk/edit
 public class Main {
-    static Roster r = new Roster();
 
     public static void main(String[] args) {
         // Leader: Cole
         // Members: Amanda, Ben, Owen
-        mainMenu(r);
+        mainMenu();
     }
 
-    private static void mainMenu(Roster r) {
+    private static void mainMenu() {
         // NOTE: Most methods should be able to be coded into the Roster class.
         // with that said, please don't code into the Main class. thx!
+        Roster r = new Roster();
         addOrDeleteStudent(r);
         changeGradeOrSchedule();
         sortStudents();
