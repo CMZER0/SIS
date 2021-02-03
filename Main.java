99a5e5555d4a04fb1e7daacbf4f6b68df4de67ca
bmcmanus0318 <bmcmanus0318@gmail.com>
2021-02-02 22:29:02 -0700

    PULL REQ REQUIREMENT

--- Main.java
+++ Main.java
@@ -7,8 +7,11 @@ public class Main {
     public static void main(String[] args) throws IOException {
         // Leader: Cole
         // Members: Amanda, Ben, Owen
+    	r.fillStudentList();
+    	r.fillCourses();
         mainMenu(r);
         System.out.println("Pull on 2.1.21");
+        System.out.println("PULL");
     }
 
     private static void mainMenu(Roster r) {
