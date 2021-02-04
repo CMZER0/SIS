7a76fe5cba11109c3474629718f1925aaf0c859f
bmcmanus0318 <bmcmanus0318@gmail.com>
2021-02-04 10:31:02 -0700

    GPA FIXED MAIN CLASS IS BROKEN

---
+++ Main.java
@@ -0,0 +1,44 @@
+import java.io.IOException;
+
+// Objectives given here: https://docs.google.com/document/d/1VyqRG5wR3q0exFC8klmNL9SVNYCZzdsOd8hYDabRpMk/edit
+public class Main {
+   
+    static Roster r = new Roster();
+
+    public static void main(String[] args) throws IOException {
+        // Leader: Cole
+        // Members: Amanda, Ben, Owen
+        
+    	System.out.println("FIXED ROSTER MAIN FOR ME BROKEN");
+
+
+
+    }
+
+   
+    private static void mainMenu(Roster r) {
+        // NOTE: Most methods should be able to be coded into the Roster class.
+        // with that said, please don't code into the Main class. thx!
+       
+    }
+
+    
+    private static void addOrDeleteStudent(Roster r) {
+        //r.addStudent();
+        // removeStudent();
+    }
+
+    private static void changeGradeOrSchedule() {
+        // changeGrade();
+        // changeSchedule();
+
+         //r.changeGrade();
+         //r.changeSchedule();
+    }
+
+    private static void sortStudents() {
+        // sortLastName();
+        // sortGPA();
+        // sortPeriod();
+    }
+}
--- Roster.java
+++ Roster.java
@@ -8,47 +8,36 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Roster {
-	
-    private ArrayList<Student> studentList = new ArrayList<>(); // ArrayList containing the list of students
+	private ArrayList<Student> studentList = new ArrayList<>(); // ArrayList containing the list of students
 
-    ///////////////////////
-    // GETTERS & SETTERS //
-    ///////////////////////
+	///////////////////////
+	// GETTERS & SETTERS //
+	///////////////////////
 
-    public ArrayList<Student> getStudentList() {
-        return studentList;
-    }
+	public ArrayList<Student> getStudentList() {
+		return studentList;
+	}
 
-    public void setStudentList(ArrayList<Student> studentList) {
-        this.studentList = studentList;
-    }
+	public void setStudentList(ArrayList<Student> studentList) {
+		this.studentList = studentList;
+	}
 
-    public Student getStudent(int index) {
-        return studentList.get(index);
-    }
+	public Student getStudent(int index) {
+		return studentList.get(index);
+	}
 
-    public void setStudent(int index, Student element) {
-        studentList.set(index, element);
-    }
+	public void setStudent(int index, Student element) {
+		studentList.set(index, element);
+	}
 
-    public void addStudent(Student e) {
-        studentList.add(e);
-    }
+	public void addStudent(Student e) {
+		studentList.add(e);
+	}
 
-    public void addStudent(int index, Student e) { // Overidden
-        studentList.add(index, e);
-    }
+	public void addStudent(int index, Student e) { // Overidden
+		studentList.add(index, e);
+	}
 
-<<<<<<< HEAD
-    public void addNewStudent(String name) {
-        studentList.add(new Student(name));
-        
-    }
-
-    public void addNewStudent(int index, String name) { // Overidden
-        studentList.add(index, new Student(name));
-    }
-=======
 	public void addNewStudent(String firstName, String lastName) {
 		studentList.add(new Student(firstName, lastName));
 	}
@@ -56,162 +45,21 @@ public class Roster {
 	public void addNewStudent(int index, String firstName, String lastName) { // Overidden
 		studentList.add(index, new Student(firstName, lastName));
 	}
->>>>>>> a14930a0b26070ceee49dd82684454432d33e536
 
-    public void removeStudent(int index) {
-        studentList.remove(index);
-    }
+	public void removeStudent(int index) {
+		studentList.remove(index);
+	}
 
-    public int classSize() {
-        return studentList.size();
-    }
-
-    //////////////
-    // METHODS //
-    /////////////
-
-<<<<<<< HEAD
-=======
 	public int getClassSize() {
 		return studentList.size();
 	}
->>>>>>> a14930a0b26070ceee49dd82684454432d33e536
 
-    
-  
+	//////////////
+	// METHODS //
+	/////////////
 
 	public void newStudentList() throws IOException // Gets, creates, and assings student names and objects
 	{
-<<<<<<< HEAD
-		Scanner studentFile = new Scanner(new File("StudentList.txt"));// Scanner that grabs the Student List
-
-		while (studentFile.hasNext()) {
-			String firstName = studentFile.next();
-			String lastName = studentFile.next();
-			studentFile.nextLine();
-			addNewStudent(firstName + " " + lastName);
-		}
-
-	}
-
-	public void fillCourses() throws IOException // Gets courses from text file and puts them in courses AL
-	{
-		Scanner courseFile = new Scanner(new File("StudentList.txt"));
-
-		for (int i = 0; i < studentList.size(); i++) {
-			courseFile.next();
-			courseFile.next();
-			String per1Class = courseFile.next();
-			String per1LetterGrade = courseFile.next();
-			String per2Class = courseFile.next();
-			String per2LetterGrade = courseFile.next();
-			String per3Class = courseFile.next();
-			String per3LetterGrade = courseFile.next();
-
-			double per1NumberGrade = gradeConverter(per1LetterGrade);
-			double per2NumberGrade = gradeConverter(per2LetterGrade);
-			double per3NumberGrade = gradeConverter(per3LetterGrade);
-
-			getStudent(i).getTranscript().add(new Course(per1Class, per1LetterGrade, per1NumberGrade));
-			getStudent(i).getTranscript().add(new Course(per2Class, per2LetterGrade, per2NumberGrade));
-			getStudent(i).getTranscript().add(new Course(per3Class, per3LetterGrade, per3NumberGrade));
-		}
-	}
-
-	public double gradeConverter(String letterGrade) // converts the given letter grade and returns the number grade(on
-														// a 4pt scale)
-
-	{
-		double numberGrade;
-		if (letterGrade.equals("A+")) {
-			numberGrade = 4.0;
-		} else if (letterGrade.equals("A")) {
-			numberGrade = 4.0;
-		} else if (letterGrade.equals("A-")) {
-			numberGrade = 3.7;
-		} else if (letterGrade.equals("B+")) {
-			numberGrade = 3.3;
-		} else if (letterGrade.equals("B")) {
-			numberGrade = 3.0;
-		} else if (letterGrade.equals("B-")) {
-			numberGrade = 2.7;
-		} else if (letterGrade.equals("C+")) {
-			numberGrade = 2.3;
-		} else if (letterGrade.equals("C")) {
-			numberGrade = 2.0;
-		} else if (letterGrade.equals("C-")) {
-			numberGrade = 1.7;
-		} else if (letterGrade.equals("D+")) {
-			numberGrade = 1.3;
-		} else if (letterGrade.equals("D")) {
-			numberGrade = 1.0;
-		} else if (letterGrade.equals("D-")) {
-			numberGrade = 0.7;
-		} else {
-			numberGrade = 0;
-		}
-
-		return numberGrade;
-	}
-
-	private static DecimalFormat df2 = new DecimalFormat("#.##");
-
-	public void displayTranscript() {
-		for (int i = 0; i < studentList.size(); i++) {
-			System.out.println();
-			System.out.printf(i + ") " + studentList.get(i).getName() + " " + studentList.get(i).getCourse(0).getName()
-					+ " " + studentList.get(i).getCourse(0).getLetterGrade() + " "
-					+ studentList.get(i).getCourse(1).getName() + " " + studentList.get(i).getCourse(1).getLetterGrade()
-					+ " " + studentList.get(i).getCourse(2).getName() + " "
-					+ studentList.get(i).getCourse(2).getLetterGrade() + " GPA: "
-					+ df2.format(studentList.get(i).getGPA()));
-
-		}
-	}
-
-	public void calculateGPA() //calculates and assigns student GPA
-    {
-	for (int i = 0; i < studentList.size(); i++) {
-		double calculatedGPA;
-		calculatedGPA = (studentList.get(i).getCourse(0).getGrade() + studentList.get(i).getCourse(1).getGrade() + studentList.get(i).getCourse(2).getGrade()) / 3;
-		studentList.get(i).setGPA(calculatedGPA);
-}
-}
-    
-    
-    
-    
-    
-
-    public void deleteStudent() {
-        displayStudents();
-        Scanner userInp = new Scanner(System.in);
-        System.out.println("Select what student you would like to delete");
-        int choice = userInp.nextInt();
-        System.out.println(getStudent(choice + 1).getName() + " will be removed.");
-        removeStudent(choice + 1);
-    }
-
-    public void addStudent() {
-        Scanner userInp = new Scanner(System.in);
-        System.out.println("What is the name of your new student?");
-        String Name = userInp.nextLine();
-        addNewStudent(Name);
-
-        for (int i = 0; i < 3; i++) {
-            int p = i + 1;
-            System.out.println("Please enter course " + p + " name:");
-            String course = userInp.nextLine();
-            System.out.println("Please enter course " + p + " grade:");
-            String grade = userInp.nextLine();
-            getStudent(classSize() - 1).getTranscript().add(new Course(course, grade, gradeConverter(grade)));
-        }
-
-        displayStudents();
-    }
-
-    
-=======
 		Scanner file = new Scanner(new File("StudentList.txt"));// Scanner that grabs the Student List
 		while (file.hasNextLine()) {
 			Scanner lineScanner = new Scanner(file.nextLine());
@@ -225,14 +73,16 @@ public class Roster {
 			s.setGPA();
 		}
 	}
->>>>>>> a14930a0b26070ceee49dd82684454432d33e536
 
 	public void sortByGPA() { // sorts student list by GPA (highest to lowest)
 		Collections.sort(studentList, new SortByGPA());
 	}
 
 	public void sortByLastName() { // sorts student list in alphabetical order by last name
+		
 		Collections.sort(studentList, new SortByLast());
+		
+		
 	}
 
 	public static String createLastName(String name) { // outputs the last name from a full name input
@@ -253,11 +103,6 @@ public class Roster {
 		Collections.sort(studentList, new SortByPeriod());
 	}
 
-<<<<<<< HEAD
-	
-
-	
-=======
 	public void deleteStudent() {
 		displayStudents();
 		try {
@@ -275,31 +120,35 @@ public class Roster {
 	public void addStudent() {
 		try {
 			Scanner userInp = new Scanner(System.in);
-			System.out.println("Input name :");
-			// addNewStudent(userInp.nextLine());
+			System.out.println("Input first name :");
+			String first = userInp.nextLine();
+			Scanner userInp2 = new Scanner(System.in);
+			System.out.println("Input last name :");
+			String last = userInp.nextLine();
+		    addNewStudent(first, last);
 		} catch (InputMismatchException e) {
 			System.out.println("Sorry, that was not a valid input.");
 			addStudent();
 		}
 
-		// for (int i = 0; i < 3; i++) {
-		// int p = i + 1;
-		// System.out.println("Please enter course " + p + " name:");
-		// String course = userInp.nextLine();
-		// System.out.println("Please enter course " + p + " grade:");
-		// String grade = userInp.nextLine();
-		// getStudent(getClassSize() - 1).getTranscript().add(new Course(course, grade,
-		// gradeConverter(grade)));
-		// }
+		 for (int i = 0; i < 3; i++) {
+		 int p = i + 1;
+		 Scanner userInp = new Scanner(System.in);
+		 System.out.println("Please enter course " + p + " name:");
+		 String course = userInp.nextLine();
+		 System.out.println("Please enter course " + p + " grade:");
+		 String grade = userInp.nextLine();
+		getStudent(getClassSize() - 1).getTranscript().add(new Course(course, grade));
+		 }
+		 getStudent(getClassSize()-1).setGPA();
 
 		displayStudents();
 	}
->>>>>>> a14930a0b26070ceee49dd82684454432d33e536
 
 	public void displayStudents() {
 		DecimalFormat df2 = new DecimalFormat("#.##");
 		for (Student s : studentList) {
-			System.out.print("\n[" + studentList.indexOf(s) + "] " + s.getName() + "| ");
+			System.out.print("\n[" + studentList.indexOf(s) + "] " + s.getfirstName() + " " + s.getLastName() + "| ");
 			for (Course c : s.getTranscript()) {
 				System.out.print(c.getName() + ": " + c.getLetterGrade() + ", ");
 			}
@@ -307,5 +156,5 @@ public class Roster {
 		}
 		System.out.print("\n\n");
 	}
-
 }
+
